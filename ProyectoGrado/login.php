dde75dbf6171ae22dc0300ef94c30588a77eb027
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-02-28 20:21:49 -0500

    Update login.php

--- ProyectoGrado/login.php
+++ ProyectoGrado/login.php
@@ -1,5 +1,5 @@
 <?php
-$_SESSION['usuario'] = "Mayerly Ortiz";
+session_start();
 
 $usuario = $_POST['identificacion'] ?? '';
 $contrasena = $_POST['contrasena'] ?? '';
@@ -9,16 +9,19 @@ $usuario_valido = "estudiante";
 $contrasena_valida = "12345";
 
 if($usuario === $usuario_valido && $contrasena === $contrasena_valida){
-    $_SESSION['usuario'] = $usuario;
+
+    $_SESSION['usuario'] = "Mayerly Ortiz";
+
     header("Location: dashboard.php");
     exit();
-    <p style="text-align:center; font-weight:500;">
-    👩‍🎓 Estudiante: <?php echo $_SESSION['usuario']; ?>
-</p>
+
 } else {
+
     echo "Credenciales incorrectas. <a href='index.php'>Volver</a>";
+
 }
 ?>
+
 
 
 
