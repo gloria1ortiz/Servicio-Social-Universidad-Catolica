ca3b577264d409bfc00448a9396e1ceeaa64258f
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-03 20:19:23 -0500

    Update horas_detalle.php

--- ProyectoGrado/horas_detalle.php
+++ ProyectoGrado/horas_detalle.php
@@ -46,7 +46,7 @@ setTimeout(function(){
     </div>
 
     <div style="text-align:center; margin-top:20px;">
-        <a href="index.php" class="btn-volver">← Volver al menú
+        <a class="volver" href="dashboard.php">⬅ Volver al menú</a>
     </div>
 
     <!-- Círculo de progreso -->
