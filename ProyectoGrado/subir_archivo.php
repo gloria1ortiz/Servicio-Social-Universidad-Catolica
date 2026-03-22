9aeb7adff3c072314c547ba9c7d9d0223e0f8c2a
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-22 14:50:34 -0500

    Update subir_archivo.php

--- ProyectoGrado/subir_archivo.php
+++ ProyectoGrado/subir_archivo.php
@@ -1,1 +1,18 @@
+<?php
+if(isset($_FILES['archivo'])){
 
+    $archivo = $_FILES['archivo']['name'];
+    $ruta_temporal = $_FILES['archivo']['tmp_name'];
+
+    // Carpeta donde se guardará
+    $ruta_destino = "uploads/" . $archivo;
+
+    // Mover archivo
+    if(move_uploaded_file($ruta_temporal, $ruta_destino)){
+        echo "Archivo subido correctamente<br>";
+        echo "<a href='pagos.php'>Volver</a>";
+    } else {
+        echo "Error al subir archivo";
+    }
+}
+?>
