09add1e2af341f8a36e2afcedc45956c5ae7ce4c
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-22 17:34:40 -0500

    Update cie.php

--- ProyectoGrado/cie.php
+++ ProyectoGrado/cie.php
@@ -36,18 +36,7 @@ session_start();
                 Seleccionar archivo
                 <input type="file" name="archivo" hidden required>
             </label>
-
-            <br><br>
-
-            <button type="submit" class="btn-verde">
-          
-            </button>
-
-        </form>
-
-        <br>
-
-       
+      
         <form action="subir_archivo.php" method="POST" enctype="multipart/form-data">
     
     <label>Subir evidencia (PDF o imagen):</label><br><br>
