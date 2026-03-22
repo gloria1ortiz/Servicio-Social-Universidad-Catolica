8497a924cae81f66534798d625d7a70fef7e2a61
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-22 13:44:03 -0500

    Update laboratorio.php with new task links

--- ProyectoGrado/laboratorio.php
+++ ProyectoGrado/laboratorio.php
@@ -29,7 +29,7 @@ session_start();
     <li>Apoyo técnico</li>
 </ul>
 
-        <a href="dashboard.php" class="btn-volver">Volver al menú</a>
+        <a href="disponibilidades.php" class="btn-volver">Volver a disponibilidades</a>
 
     </div>
 
