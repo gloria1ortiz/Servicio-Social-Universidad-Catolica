73e96d4ac541aecdb6bba12820a3ba729c32cf70
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-22 19:33:24 -0500

    Update eliminar.php

--- ProyectoGrado/eliminar.php
+++ ProyectoGrado/eliminar.php
@@ -1,1 +1,22 @@
+<?php
+session_start();
 
+if(isset($_GET['archivo'])){
+
+    $archivo = $_GET['archivo'];
+    $ruta = "uploads/" . $archivo;
+
+    // Borrar archivo físico
+    if(file_exists($ruta)){
+        unlink($ruta);
+    }
+
+    // Borrar de la sesión
+    if(($key = array_search($archivo, $_SESSION['archivos'])) !== false){
+        unset($_SESSION['archivos'][$key]);
+    }
+
+    header("Location: cie.php");
+    exit();
+}
+?>
