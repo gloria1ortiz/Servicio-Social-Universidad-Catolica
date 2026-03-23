49e0e0025b4c3235de7a5190171d963ae9aa2fc4
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-03-22 19:20:28 -0500

    Update cie.php

--- ProyectoGrado/cie.php
+++ ProyectoGrado/cie.php
@@ -1,36 +1,3 @@
-<?php
-session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>CIE</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-
-<body>
-
-<div class="dashboard-container">
-
-    <div class="dashboard-header">
-        Módulo CIE
-    </div>
-
-    <div class="dashboard-body">
-
-        <h3>Tareas disponibles</h3>
-
-        <ul>
-            <li>Migración de cursos</li>
-            <li>Revisión de diseños</li>
-            <li>Apoyo en contenidos educativos</li>
-        </ul>
-
-  <p><strong>Adjuntar evidencia:</strong></p>
-
-          
 <form action="subir_archivo.php" method="POST" enctype="multipart/form-data">
 
     <label class="btn-verde">
@@ -46,19 +13,23 @@ session_start();
 
 </form>
 
-       <?php if(isset($archivo_subido)){ ?>
-    <p>📄 Archivo subido:</p>
-    <a href="uploads/<?php echo $archivo_subido; ?>" target="_blank" class="btn-verde">
-        Ver evidencia
-    </a>
-<?php } ?> 
+<br>
 
-                <a href="pagos.php" class="btn-volver">Volver a disponibilidades</a>
-    </div>
+<!-- 👇 AQUÍ VA LO QUE ME PREGUNTASTE -->
+<?php if(isset($_SESSION['archivos'])){ ?>
 
-</div>
+    <h4>📄 Evidencias subidas:</h4>
 
-</body>
-</html>
+    <?php foreach($_SESSION['archivos'] as $archivo){ ?>
 
-  
+        <a href="uploads/<?php echo $archivo; ?>" target="_blank" class="btn-verde">
+            Ver evidencia
+        </a>
+        <br><br>
+
+    <?php } ?>
+
+<?php } ?>
+
+<!-- 👇 BOTÓN VOLVER -->
+<a href="pagos.php" class="btn-volver">⬅ Volver a disponibilidades</a>
