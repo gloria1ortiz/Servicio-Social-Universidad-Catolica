e1b6855dc5d7b9a3e9359b28646c83f9aa9b04f8
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-04-09 21:33:59 -0500

    Update conexion.php

--- ProyectoGrado/conexion.php
+++ ProyectoGrado/conexion.php
@@ -1,6 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "proyecto_grado");
-
+$conexion = mysqli_connect("localhost", "root", "root", "proyecto_grado");
 if (!$conexion) {
     die("Error de conexión");
 }
