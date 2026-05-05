0a635c33dcd6ebddbe8c687576836ed21b0d51d3
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-05-04 20:45:22 -0500

    Update login.php

--- ProyectoGrado/login.php
+++ ProyectoGrado/login.php
@@ -1,20 +1,9 @@
 <?php
 session_start();
+
+$_SESSION['usuario'] = "Gloria";
 include("conexion.php");
-
-if(!empty($_POST['usuario']) && !empty($_POST['password'])){
-
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
-
-    if($usuario === "estudiante" && $password === "12345"){
-        $_SESSION['usuario'] = $usuario;
-
-        // 🔥 FORZAR REDIRECCIÓN
-        echo "<script>window.location='dashboard.php';</script>";
-        exit();
-    } else {
-        $error = "Usuario o contraseña incorrectos";
-    }
-}
+header("Location: dashboard.php");
+exit();
+?>
 ?>
