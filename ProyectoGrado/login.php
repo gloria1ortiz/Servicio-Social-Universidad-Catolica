98f6808c0cbc1d026fac43502eea74e7a3af76cc
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-05-04 21:06:42 -0500

    Update login.php

--- ProyectoGrado/login.php
+++ ProyectoGrado/login.php
@@ -1,37 +1,8 @@
 <?php
 session_start();
 
-if(isset($_POST['entrar'])){
-    $_SESSION['usuario'] = "Gloria";
-    header("Location: dashboard.php");
-    exit();
-}
+$_SESSION['usuario'] = "Gloria";
+include("conexion.php");
+header("Location: dashboard.php");
+exit();
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Portal Estudiantil</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-
-<div class="login-container">
-
-    <div class="login-header">
-        Portal Estudiantil
-    </div>
-
-    <form method="POST">
-
-        <button type="submit" name="entrar" class="btn-login">
-            ENTRAR
-        </button>
-
-    </form>
-
-</div>
-
-</body>
-</html>
