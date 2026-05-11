542445e501c69da2d1e7547175b753dfc08d1838
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-05-11 18:08:48 -0500

    Update laboratorio.php

--- ProyectoGrado/laboratorio.php
+++ ProyectoGrado/laboratorio.php
@@ -52,7 +52,9 @@ $resultado = mysqli_query($conexion, $sql);
         Módulo Laboratorio
     </div>
 
-    <div class="dashboard-body">
+   <div class="dashboard-body">
+
+    <div class="modulo-content">
 
         <h3>Tareas disponibles</h3>
 
