98e7f9933e89df965f04830264043555248309fc
gloria1ortiz <gloria1.ortiz@ucp.edu.co>
2026-05-11 19:11:06 -0500

    Refactor event actions in eventos.php

--- ProyectoGrado/eventos.php
+++ ProyectoGrado/eventos.php
@@ -107,15 +107,20 @@ $resultado = mysqli_query($conexion, $sql);
 
                 <strong>Actividad:</strong> <?php echo $row['actividad']; ?><br><br>
 
-                 <a href="uploads/<?php echo $row['archivo']; ?>" target="_blank" class="btn-verde">
-                    📄 Ver
-                </a>
+                <div class="acciones">
 
-               <a href="eliminar.php?id=<?php echo $row['id']; ?>&tipo=eventos" class="btn-rojo">
-                    🗑 Eliminar
-                </a>
+    <a href="uploads/<?php echo $fila['archivo']; ?>" 
+       target="_blank" 
+       class="btn-verde">
+       📄 Ver
+    </a>
 
-            </div>
+    <a href="eliminar.php?id=<?php echo $fila['id']; ?>&tipo=laboratorio" 
+       class="btn-rojo">
+       🗑 Eliminar
+    </a>
+
+</div>
 
         <?php } ?>
 
